20328717312b8fe5257a798ddaf39146cd03d1ae
xhacu <xhacubejaj@gmail.com>
2025-11-23 12:46:16 +0100

    cleaned main branch

--- app/services/abstract_client.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from app.models.schemas import GenerateResponse
-
-
-class GenerativeAIClient(ABC):
-  @abstractmethod
-  def generate(self, prompt: str, **kwargs) -> GenerateResponse:
-      ...
